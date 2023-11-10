3e89412182903c426b40e663977bf7645468d93a
danield424 <danieldu42466@gmail.com>
2023-11-10 15:58:07 -0500

    restructured files

--- src/entity/PrivateChat.java
+++ src/entity/PrivateChat.java
@@ -1,4 +1,13 @@
 package entity;
 
+import interface_adapter.PrivateChat.MessageHistory;
+
 public class PrivateChat {
+
+    private User user1;
+
+    private User user2;
+
+    private MessageHistory messages;
+
 }
