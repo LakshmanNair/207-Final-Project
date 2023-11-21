27412eaa2563d2b897dd229e966d9cc881bd3f99
danield424 <danieldu42466@gmail.com>
2023-11-20 20:34:56 -0500

    test

--- src/entity/UserFactory.java
+++ src/entity/UserFactory.java
@@ -1,10 +1,7 @@
 package entity;
 
-<<<<<<< aamya
-
 public class UserFactory implements UserFactoryInterface {
     public User createUser(String username, String password) {
->>>>>>> main
         return new User(username, password);
     }
 }
