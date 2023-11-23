ab5dd6b7082c4b2d8e10397cbf88ed7a37e88691
areezbhanji <areez.bhanji@mail.utoronto.ca>
2023-11-23 18:26:03 -0500

    finished AccountFileDataAccessObject minor updates to use_case/edit_account_information resolving merge conflicts added SetUserid to user class to ensure userids are constant when loading users from csv file

--- src/use_case/edit_account_information/EditInputData.java
+++ src/use_case/edit_account_information/EditInputData.java
@@ -2,22 +2,32 @@ package use_case.edit_account_information;
 
 public class EditInputData {
     private final String userid;
+    private final String currentUsername;
+    private final String currentPassword;
     private final String newUsername;
     private final String newPassword;
 
-    public EditInputData(String userid, String newUsername, String newPassword) {
+    public EditInputData(String userid, String currentUsername, String currentPassword,
+                         String newUsername, String newPassword) {
         this.userid = userid;
+        this.currentUsername = currentUsername;
+        this.currentPassword = currentPassword;
         this.newUsername = newUsername;
         this.newPassword = newPassword;
     }
-
-    String getUserid() {
+    public String getUserid() {
         return userid;
     }
-    String getNewUsername() {
+    public String getCurrentUsername() {
+        return currentUsername;
+    }
+    String getCurrentPassword() {
+        return currentPassword;
+    }
+    public String getNewUsername() {
         return newUsername;
     }
-    String getNewPassword() {
+    public String getNewPassword() {
         return newPassword;
     }
 }
