f2c5bb8b0dd7508ed9e841633dd4132ed24a1841
aamyahanda <aamya.handa@gmail.com>
2023-11-23 19:44:01 -0500

    added to interface_adapter, CreateAccountPresenter, CreateAccountState, CreateAccountController, CreateAccountView, ViewModel

---
+++ src/view/CreateAccountView.java
@@ -0,0 +1,17 @@
+package view;
+
+//import interface_adapter.PrivateChat.;
+//import interface_adapter.create_account.SignupState;
+//import interface_adapter.signup.SignupViewModel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class CreateAccountView{
+}
