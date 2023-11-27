21eb1be7e725eff121133c8de9f20dffd21910d6
areezbhanji <areez.bhanji@mail.utoronto.ca>
2023-11-27 14:36:57 -0500

    updated use_case/create_privatechat so the interactor creates a queue with a given subject and url when execute is called

--- src/use_case/create_privatechat/CreatePCInteractor.java
+++ src/use_case/create_privatechat/CreatePCInteractor.java
@@ -1,4 +1,32 @@
 package use_case.create_privatechat;
 
-public class CreatePCInteractor {
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Session;
+import javax.jms.Destination;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class CreatePCInteractor implements CreatePCInputBoundary {
+    final CreatePCOutputBoundary createPCPresenter;
+    public CreatePCInteractor(CreatePCOutputBoundary createPCPresenter) {
+        this.createPCPresenter = createPCPresenter;
+    }
+    @Override
+    public void execute(CreatePCInputData createPCInputData) {
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(CreatePCInputData.getUrl());
+        try {
+            Connection connection = connectionFactory.createConnection();
+            connection.start();
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Destination destination = session.createQueue(CreatePCInputData.getSubject());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        CreatePCOutputData createPCOutputData = new CreatePCOutputData(CreatePCInputData.getUrl(),
+                CreatePCInputData.getSubject());
+        createPCPresenter.prepareSuccessView(createPCOutputData);
+    }
 }
