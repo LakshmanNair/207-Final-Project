44b77f91efda4ee778081c01701ae204e47d0c2c
shenxi90 <xian.shen@mail.utoronto.ca>
2023-12-01 06:51:36 -0500

    更改

---
+++ src/interface_adapter/groupChat/GroupChatState.java
@@ -0,0 +1,20 @@
+package interface_adapter.groupChat;
+
+import entity.User;
+import interface_adapter.logged_in.LoggedInState;
+
+public class GroupChatState {
+    private User user;
+
+    public GroupChatState(GroupChatState copy) {
+        user = copy.user;
+    }
+
+    // Because of the previous copy constructor, the default constructor must be explicit.
+    public GroupChatState() {}
+    public User getUser(){return user;}
+    public String getUsername() {
+        return user.getUsername();
+    }
+    public void setUsername(String username) {user.setUsername(username);}
+}
