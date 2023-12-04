bb78edb0752758ca7b25717ed617fe55d9c60230
danield424 <danieldu42466@gmail.com>
2023-12-04 12:16:31 -0500

    some bug fixes

--- src/entity/User.java
+++ src/entity/User.java
@@ -3,21 +3,13 @@ package entity;
 import interface_adapter.PrivateChat.CreateUserid;
 
 public class User implements UserInterface{
-    private String userid;
     private String username;
     private String password;
 
     public User(String username, String password) {
-        this.userid = setUserID();// the next one available. store list of user ids.;
         this.username = username;
         this.password = password;
     }
-
-    @Override
-    public String getUserID() {
-        return userid;
-    }
-
     @Override
     public String getUsername() {
         return username;
@@ -28,15 +20,10 @@ public class User implements UserInterface{
         return password;
     }
 
-    public String setUserID() {
-        return CreateUserid.NewUserID().toString();// next unused ID or token
-    }
     public void setUsername(String new_username) {
         this.username = new_username;
     }
     public void setPassword(String new_password) {
         this.password = new_password;
     }
-    //public void setUserid(String new_userid) {this.userid = new_userid;}
-
 }
