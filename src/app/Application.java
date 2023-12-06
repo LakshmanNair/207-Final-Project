15640bddc4ca596cafa0b784e1c55381d854eab2
shenxi90 <xian.shen@mail.utoronto.ca>
2023-12-05 20:12:50 -0500

    add method contain

--- src/app/Application.java
+++ src/app/Application.java
@@ -111,6 +111,42 @@ public class Application extends JPanel {
 
         });
     };
+
+    public static void showGroupChat(String username) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    APIAccessObject apiAccessObject = new APIAccessObject(ActiveMQConnection.DEFAULT_BROKER_URL);
+                    SendMessageInteractor sendMessageInteractor = new SendMessageInteractor(apiAccessObject);
+                    PrivateChatView chatView = new PrivateChatView();
+                    PrivateChatPresenter chatPresenter = new PrivateChatPresenter(chatView);
+                    PrivateChatController chatController = new PrivateChatController(sendMessageInteractor, new User(username, "pass"), new User("tester2", "pass"), apiAccessObject, chatView);
+
+                    sendMessageInteractor.setOutputBoundary(chatPresenter);
+                    chatView.setController(chatController);
+                    ReceiveMessageWorker receiveMessageWorker = new ReceiveMessageWorker(apiAccessObject, "tester2", chatView);
+                    receiveMessageWorker.execute();
+                    chatView.setVisible(true);
+                } catch (JMSException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+                try {
+                    APIAccessObject apiAccessObject = new APIAccessObject(ActiveMQConnection.DEFAULT_BROKER_URL);
+                    SendMessageInteractor sendMessageInteractor = new SendMessageInteractor(apiAccessObject);
+
+                    GroupChatState groupChatState=new GroupChatState(new User(username,""));
+                    GroupChatViewModel groupChatViewModel=new GroupChatViewModel(groupChatState);
+                    GroupChatView groupChatView=GroupChatCaseFactory.create(groupChatViewModel,sendMessageInteractor,apiAccessObject);
+                    sendMessageInteractor.setOutputBoundary(groupChatView);
+                    groupChatView.setVisible(true);
+                } catch (JMSException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+
+        });
+    }
 }
 //
 
